c891424215e0cd948b2e34ac9f73b6de9dfcd62c
Tanakaryuki <tnk3257.rt@gmail.com>
2023-10-16 01:03:01 +0900

    fix: #2 スキーマを修正

--- api/schemas/event.py
+++ api/schemas/event.py
@@ -4,30 +4,30 @@ from user import UserNavigateResponse
 
 
 class EventDraftRequest(BaseModel):
-    administrator_id: str = Field(..., examples="admin", description="User.id")
-    title: str | None = Field(None, examples="抽選で10名様に美食ツアーをプレゼント")
+    administrator_id: str = Field(..., example="admin", description="User.id")
+    title: str | None = Field(None, example="抽選で10名様に美食ツアーをプレゼント")
     image_url: HttpUrl = Field(...,
-                               examples="https://storage.googleapis.com/...")
-    tags: list[str] | None = Field(None, examples=["ツアー", "グルメ体験"])
-    winning_number: int = Field(..., examples=10)
-    start_time: datetime = Field(..., examples=1701442800,
+                               example="https://storage.googleapis.com/...")
+    tags: list[str] | None = Field(None, example=["ツアー", "グルメ体験"])
+    winning_number: int = Field(..., example=10)
+    start_time: datetime = Field(..., example=1701442800,
                                  description="UNIXTIME")
-    end_time: datetime = Field(..., examples=1701486000,
+    end_time: datetime = Field(..., example=1701486000,
                                description="UNIXTIME")
-    detail: str = Field(..., examples="抽選で10名の幸運な参加者に、豪華な美食ツアーをプレゼントします!!")
-    id: str = Field(..., examples="oishi_o_123", description="Event.id")
+    detail: str = Field(..., example="抽選で10名の幸運な参加者に、豪華な美食ツアーをプレゼントします!!")
+    id: str = Field(..., example="oishi_o_123", description="Event.id")
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 
 class EventPublicationRequest(BaseModel):
-    id: str = Field(..., examples="oishi_o_123", description="Event.id")
+    id: str = Field(..., example="oishi_o_123", description="Event.id")
 
 
 class EventRegistrationRequest(BaseModel):
-    id: str = Field(..., examples="oishi_o_123", description="Event.id")
-    participant_id: str = Field(..., examples="admin", description="User.id")
+    id: str = Field(..., example="oishi_o_123", description="Event.id")
+    participant_id: str = Field(..., example="admin", description="User.id")
 
 
 class EventListItem(BaseModel):
--- api/schemas/user.py
+++ api/schemas/user.py
@@ -3,25 +3,25 @@ from datetime import datetime
 
 
 class UserSignupRequest(BaseModel):
-    email: EmailStr = Field(..., examples="example@example.com")
-    password: str = Field(..., examples="password")
-    id: str = Field(..., examples="admin")
-    display_name: str = Field(..., examples="福岡太郎")
-    age: int = Field(..., examples=20)
-    phone_number: str = Field(..., examples="070-0123-4567")
-    address: str = Field(..., examples="〒123-4567 東京都港区浜松町1-2-3 サクラビル205号室")
+    email: EmailStr = Field(..., example="example@example.com")
+    password: str = Field(..., example="password")
+    id: str = Field(..., example="admin")
+    display_name: str = Field(..., example="福岡太郎")
+    age: int = Field(..., example=20)
+    phone_number: str = Field(..., example="070-0123-4567")
+    address: str = Field(..., example="〒123-4567 東京都港区浜松町1-2-3 サクラビル205号室")
     introduction: str | None = Field(
-        examples="グルメフェア株式会社は、美味しさと幸せを提供することに情熱を傾ける食品会社です。")
-    sns_url: HttpUrl | None = Field(examples="https://x.com/Elonmusk")
-    homepage_url: str | None = Field(examples="https://example.com")
+        example="グルメフェア株式会社は、美味しさと幸せを提供することに情熱を傾ける食品会社です。")
+    sns_url: HttpUrl | None = Field(example="https://x.com/Elonmusk")
+    homepage_url: str | None = Field(example="https://example.com")
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 
 class UserSigninRequest(BaseModel):
-    email: EmailStr = Field(..., examples="example@example.com")
-    password: str = Field(..., examples="password")
+    id: str = Field(..., example="admin")
+    password: str = Field(..., example="password")
 
 
 class UserNavigateResponse(BaseModel):
