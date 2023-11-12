7e1a3afe67c75672ea61619bf511af9238736705
Tanakaryuki <tnk3257.rt@gmail.com>
2023-11-13 01:00:46 +0900

    add: #5 サインイン機能を実装

--- api/cruds/user.py
+++ api/cruds/user.py
@@ -1,4 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from sqlalchemy.engine import Result
+
 import api.models.user as user_model
 import api.schemas.user as user_schema
 
@@ -6,7 +9,7 @@ from passlib.context import CryptContext
 
 
 async def creatr_user(db: AsyncSession, signup: user_schema.UserSignupRequest) -> user_model.User | None:
-    signup_dict = signup.dict()
+    signup_dict = signup.model_dump()
     signup_dict.pop("password", None)
     hashed_password = CryptContext(["bcrypt"]).hash(signup.password)
     user = user_model.User(
@@ -15,3 +18,12 @@ async def creatr_user(db: AsyncSession, signup: user_schema.UserSignupRequest) -
     await db.commit()
     await db.refresh(user)
     return user
+
+
+async def read_user_by_id(db: AsyncSession, id: str) -> user_model.User | None:
+    result: Result = await (db.execute(select(user_model.User).filter(user_model.User.id == id)))
+    user: user_model.User | None = result.first()
+    if user is None:
+        return None
+    else:
+        return user[0]
--- api/routers/user.py
+++ api/routers/user.py
@@ -1,15 +1,22 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi.security import OAuth2PasswordRequestForm
+from jose import jwt
+from passlib.context import CryptContext
+import os
+from dotenv import load_dotenv
 
 import api.schemas.user as user_schema
 import api.cruds.user as user_crud
+import api.models.user as user_model
 from api.db import get_db
 
+load_dotenv()
 router = APIRouter()
 
 
 @router.post("/signup", description="新しいアカウントを作成するために使用されます。", tags=["users"])
-async def singup(request: user_schema.UserSignupRequest, db: AsyncSession = Depends(get_db)):
+async def signup(request: user_schema.UserSignupRequest, db: AsyncSession = Depends(get_db)):
     user = await user_crud.creatr_user(db, request)
     if not user:
         raise HTTPException(status.HTTP_400_BAD_REQUEST)
@@ -18,5 +25,15 @@ async def singup(request: user_schema.UserSignupRequest, db: AsyncSession = Depe
 
 
 @router.post("/signin", description="既存ユーザがアカウントにサインインするために使用されます。", tags=["users"], response_model=user_schema.UserTokenResponse)
-def singup(request: user_schema.UserSigninRequest, db: AsyncSession = Depends(get_db)):
-    pass
+async def signin(request: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_db)) -> user_schema.UserTokenResponse:
+    user = await user_crud.read_user_by_id(db, id=request.username)
+    if not user:
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED)
+    if not CryptContext(["bcrypt"]).verify(request.password, user.hashed_password):
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED)
+    token = user_schema.UserTokenResponse(
+        access_token=jwt.encode(
+            {"sub": request.username}, os.environ["SECRET_KEY"], "HS256")
+    )
+
+    return token
