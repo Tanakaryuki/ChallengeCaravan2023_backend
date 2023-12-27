2e76a734e3ad351a655a733b552a2cbed3e8d67b
Tanakaryuki <tnk3257.rt@gmail.com>
2023-12-28 01:31:04 +0900

    fix: CROSを許可

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.routers import event, user
 
@@ -9,3 +10,14 @@ load_dotenv()
 app = FastAPI()
 app.include_router(event.router, prefix="/api")
 app.include_router(user.router, prefix="/api")
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
